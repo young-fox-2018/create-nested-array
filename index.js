e7bf9c32a10d91d6b51c87ebf78db15441992a86
fauzan_bintang <obin.aji@gmail.com>
2018-10-31 10:13:32 +0700

    create-nested-array done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function nested(row, col) {
     for (let i = 0; i < row; i++) {
         result.push([]);
         for (let j = 0; j < col; j++) {
-            result[i].push(alphabet[Math.floor(Math.random() * 24) + 1]);
+            result[i].push(alphabet[Math.floor(Math.random() * 25)]);
         }
     }
     return result;
